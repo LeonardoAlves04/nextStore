19231405c4db3f2dd19ea587d1ebc9f00609d0e0
unknown <leonardoalves201510@gmail.com>
2023-10-24 13:56:27 -0300

    feat: add ProductItem and ProductList

--- next_store/src/app/(home)/components/product-list.tsx
+++ next_store/src/app/(home)/components/product-list.tsx
@@ -1,5 +1,6 @@
 import { Product } from "@prisma/client";
 import ProductItem from "@/components/ui/product-item";
+import { computeProductTotalPrice } from "@/helpers/product";
 
 interface ProductListProps {
   products: Product[];
@@ -9,7 +10,10 @@ const ProductList = ({ products }: ProductListProps) => {
   return (
     <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
       {products.map((product) => (
-        <ProductItem key={product.id} product={product} />
+        <ProductItem
+          key={product.id}
+          product={computeProductTotalPrice(product)}
+        />
       ))}
     </div>
   );
--- next_store/src/components/ui/product-item.tsx
+++ next_store/src/components/ui/product-item.tsx
@@ -1,14 +1,17 @@
+import { ProductWithTotalPrice } from "@/helpers/product";
 import { Product } from "@prisma/client";
+import { ArrowDownIcon } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
 import Image from "next/image";
 
 interface ProductItemProps {
-  product: Product;
+  product: ProductWithTotalPrice;
 }
 
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
     <div className="flex max-w-[156px] flex-col gap-4">
-      <div className="flex h-[170px] w-[156px] items-center justify-center rounded-lg bg-accent">
+      <div className=" relative flex h-[170px] w-[156px] items-center justify-center rounded-lg bg-accent">
         <Image
           src={product.imageUrls[0]}
           height={0}
@@ -18,12 +21,36 @@ const ProductItem = ({ product }: ProductItemProps) => {
           style={{ objectFit: "contain" }}
           alt={product.name}
         />
+
+        {product.discountPercentage > 0 && (
+          <Badge className="absolute left-2 top-2 px-2 py-[2px]">
+            <ArrowDownIcon size={14} /> {product.discountPercentage} %
+          </Badge>
+        )}
       </div>
 
-      <div>
+      <div className="flex flex-col gap-1">
         <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
           {product.name}
         </p>
+
+        <div className="flex items-center gap-2">
+          {product.discountPercentage > 0 ? (
+            <>
+              <p className="font-semibold">
+                R$ {product.totalPrice.toFixed(2)}
+              </p>
+
+              <p className="text-xs line-through opacity-75">
+                R$ {Number(product.basePrice).toFixed(2)}
+              </p>
+            </>
+          ) : (
+            <p className="text-sm font-semibold">
+              R$ {product.basePrice.toFixed(2)}
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
