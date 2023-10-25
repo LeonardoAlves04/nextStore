27095d2ae2bbd3f972cc8d9b494e8c96707a81d4
Leonardo Alves <leonardoalves201510@gmail.com>
2023-10-24 21:40:26 -0300

    feat: add page to show category items

--- next_store/src/app/(home)/components/category-item.tsx
+++ next_store/src/app/(home)/components/category-item.tsx
@@ -1,34 +1,18 @@
 import { Badge } from "@/components/ui/badge";
+import { CATEGORY_ICON } from "@/constants/categoryItem";
 import { Category } from "@prisma/client";
-import {
-  HeadphonesIcon,
-  KeyboardIcon,
-  MonitorIcon,
-  MouseIcon,
-  SpeakerIcon,
-  SquareIcon,
-} from "lucide-react";
 
 interface CategoryItemProps {
   category: Category;
 }
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
-  const categoryIcon = {
-    keyboards: <KeyboardIcon size={18} />,
-    monitors: <MonitorIcon size={18} />,
-    mouses: <MouseIcon size={18} />,
-    headphones: <HeadphonesIcon size={18} />,
-    mousepads: <SquareIcon size={18} />,
-    speakers: <SpeakerIcon size={18} />,
-  };
-
   return (
     <Badge
       variant="outline"
       className="flex items-center justify-center gap-2 rounded-lg py-3"
     >
-      {categoryIcon[category.slug as keyof typeof categoryIcon]}
+      {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
       <span className="text-xs font-bold">{category.name}</span>
     </Badge>
   );
