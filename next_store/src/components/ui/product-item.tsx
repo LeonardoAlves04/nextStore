e2627f196a4d69ac79fbf524ba363ef59c580465
Leonardo Alves <leonardoalves201510@gmail.com>
2023-11-20 21:47:21 -0300

    fix: fixing product-item sizes

--- next_store/src/components/ui/product-item.tsx
+++ next_store/src/components/ui/product-item.tsx
@@ -10,14 +10,14 @@ interface ProductItemProps {
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
     <Link href={`/product/${product.slug}`}>
-      <div className="flex flex-col gap-4">
-        <div className=" relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
+      <div className="flex max-w-[170px] flex-col gap-4">
+        <div className=" relative flex h-[170px] w-[160px] items-center justify-center rounded-lg bg-accent">
           <Image
             src={product.imageUrls[0]}
             height={0}
             width={0}
             sizes="100vw"
-            className="h-auto max-h-[70%] w-auto max-w-[80%]"
+            className="h-auto max-h-[50%] w-auto max-w-[60%]"
             style={{ objectFit: "contain" }}
             alt={product.name}
           />
@@ -34,7 +34,7 @@ const ProductItem = ({ product }: ProductItemProps) => {
             {product.name}
           </p>
 
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 overflow-hidden text-ellipsis whitespace-nowrap">
             {product.discountPercentage > 0 ? (
               <>
                 <p className="font-semibold">
