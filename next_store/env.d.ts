224941e2629a33b48d967c12d6af20ca0cff479b
Leonardo Alves <leonardoalves201510@gmail.com>
2023-11-24 10:18:45 -0300

    feat: add stripe checkout to cart

--- next_store/env.d.ts
+++ next_store/env.d.ts
@@ -1,7 +1,9 @@
 declare namespace NodeJS {
-    interface ProcessEnv {
+  interface ProcessEnv {
     DATABASE_URL: string;
     GOOGLE_CLIENT_ID: string;
     GOOGLE_CLIENT_SECRET: string;
-    }
+    STRIPE_SECRET_KEY: string;
+    NEXT_PUBLIC_STRIPE_PUBLIC_KEY: string;
+  }
 }
