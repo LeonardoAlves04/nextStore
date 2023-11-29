eb8ac21cfc158d45feb6a8cdcd7d840f25303a70
Leonardo Alves <leonardoalves201510@gmail.com>
2023-11-29 19:58:49 -0300

    fix: fixing stripe fetch error

--- next_store/src/app/api/order/payment-success/route.ts
+++ next_store/src/app/api/order/payment-success/route.ts
@@ -28,6 +28,7 @@ export const POST = async (request: Request) => {
       },
     );
     const lineItems = sessionWithLineItems.line_items;
+    console.log(lineItems);
   }
 
   return NextResponse.json({ receveid: true });
