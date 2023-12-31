9fe4f517e94fcdb87db0eceff9e664969dd3f14d
Leonardo Alves <leonardoalves201510@gmail.com>
2023-12-31 12:04:07 -0300

    style: styling webkit scroll bar

--- next_store/src/components/ui/product-list.tsx
+++ next_store/src/components/ui/product-list.tsx
@@ -8,7 +8,7 @@ interface ProductListProps {
 
 const ProductList = ({ products }: ProductListProps) => {
   return (
-    <div className="flex w-full gap-4 overflow-x-auto px-5 lg:justify-center lg:overflow-scroll lg:scroll-smooth [&::-webkit-scrollbar]:hidden">
+    <div className="flex w-full gap-4 overflow-x-auto px-5 lg:justify-center">
       {products.map((product) => (
         <div key={product.id} className="w-[170px] max-w-[170px]">
           <ProductItem product={computeProductTotalPrice(product)} />
